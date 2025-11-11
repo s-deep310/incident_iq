931ce5ae1298895f385c6f1f0ce40eb4095e8a18
s-deep310 <samyadeep.kuila98@gmail.com>
2025-11-11 23:53:09 +0530

    Update and rename seed_users.py to 001_seed_users.py

--- database/seeders/001_seed_users.py
+++ database/seeders/001_seed_users.py
@@ -10,7 +10,7 @@ def run(conn):
     )
     conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Souvik', 'svk@example.com', _hash('souvik123')))
     conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Sourav', 'srv@example.com', _hash('sourav123')))
-    conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Prasun', 'prsb@example.com', _hash('prasun123')))
+    conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Prasun', 'prs@example.com', _hash('prasun123')))
     conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Rupankar', 'rpk@example.com', _hash('rupankar123')))
     conn.execute("INSERT OR IGNORE INTO users (name, email, password) VALUES (?, ?, ?)", ('Samya', 'sdk@example.com', _hash('samya123')))
     conn.commit()
