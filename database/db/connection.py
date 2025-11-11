bffd0e340daea1e18c0c36ff911b654773809f27
s-deep310 <samyadeep.kuila98@gmail.com>
2025-11-11 21:00:20 +0530

    Update connection.py

--- database/db/connection.py
+++ database/db/connection.py
@@ -12,7 +12,7 @@ DB_PATH = Path(__file__).resolve().parent.parent / "data" / DB_NAME
 
 def get_connection():
     os.makedirs(DB_PATH.parent, exist_ok=True)
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH,check_same_thread=False)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON;")
     return conn
