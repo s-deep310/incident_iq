95e3c5ee25183ae99a4589100527d698a987b73a
s-deep310 <samyadeep.kuila98@gmail.com>
2025-11-12 21:53:22 +0530

    Update seed_user_roles.py

--- database/seeders/seed_user_roles.py
+++ database/seeders/seed_user_roles.py
@@ -6,8 +6,29 @@ def run(conn):
         conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'], 1))
 
     # Alice → Employee
-    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('souvik@example.com',)).fetchone()
+    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('svk@example.com',)).fetchone()
+    cur_role = conn.execute("SELECT id FROM roles WHERE name = ?", ('manager',)).fetchone()
+    if cur_user and cur_role:
+        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'],2))
+    
+    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('srv@example.com',)).fetchone()
+    cur_role = conn.execute("SELECT id FROM roles WHERE name = ?", ('devloper',)).fetchone()
+    if cur_user and cur_role:
+        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'],2))
+
+    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('prs@example.com',)).fetchone()
     cur_role = conn.execute("SELECT id FROM roles WHERE name = ?", ('employee',)).fetchone()
     if cur_user and cur_role:
-        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id) VALUES (?, ?)', (cur_user['id'], cur_role['id']))
+        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'],2))
+
+    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('rpk@example.com',)).fetchone()
+    cur_role = conn.execute("SELECT id FROM roles WHERE name = ?", ('devloper',)).fetchone()
+    if cur_user and cur_role:
+        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'],2))
+
+    cur_user = conn.execute("SELECT id FROM users WHERE email = ?", ('sdk@example.com',)).fetchone()
+    cur_role = conn.execute("SELECT id FROM roles WHERE name = ?", ('employee',)).fetchone()
+    if cur_user and cur_role:
+        conn.execute('INSERT OR IGNORE INTO user_roles (user_id, role_id, company_id) VALUES (?, ?, ?)', (cur_user['id'], cur_role['id'],2))
+
     conn.commit()
