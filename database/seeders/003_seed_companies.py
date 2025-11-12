1cc9b97993f474727df2cdc6911d8fc187e4102c
s-deep310 <samyadeep.kuila98@gmail.com>
2025-11-12 21:55:05 +0530

    Update 003_seed_companies.py

--- database/seeders/003_seed_companies.py
+++ database/seeders/003_seed_companies.py
@@ -1,3 +1,5 @@
+table_name = "companies"
+
 def run(conn):
     conn.execute("INSERT OR IGNORE INTO companies (id, name) VALUES (1, 'Root_Company')")
     conn.execute("INSERT OR IGNORE INTO companies (name) VALUES (?)", ('Acme Corp',))
